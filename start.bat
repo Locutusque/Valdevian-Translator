fcb293c20ca3fb3d48ffd1995723c98298eb9aab
Locutusque <122424773+Locutusque@users.noreply.github.com>
2023-04-01 18:02:56 -0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,9 +1,10 @@
 @echo off
 echo Powering on the Valdevian Translator...
 pause
-py -m pip install transformers
-py -m pip install torch
-py -m pip install Flask
+for /f "tokens=*" %%a in ('python -c "import os, sys; print(os.path.dirname(sys.executable))"') do set PY=%%a
+"%PY%\python.exe" -m pip install transformers
+"%PY%\python.exe" -m pip install torch
+"%PY%\python.exe" -m pip install Flask
 
 set current_dir=%cd%
 
@@ -13,7 +14,7 @@ echo ^| In your browser type               ^|
 echo ^| http://localhost:5000/             ^|
 echo ^|____________________________________^|
 color 1F
-call python %current_dir%\py\backend.py
+call "%PY%\python.exe" %current_dir%\py\backend.py
 timeout /t 5 /nobreak > NUL
 
 pause
